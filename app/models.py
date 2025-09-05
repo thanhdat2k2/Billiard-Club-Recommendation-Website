0ada34a539cd4f38739b6ba932e98d5cbd25dd95
thanhdat2k2 <thanhdat2k2.810@gmail.com>
2025-09-05 14:56:35 +0700

    fill card

--- app/models.py
+++ app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Club(models.Model):
+    name = models.CharField(max_length=200,null=True)
+    district = models.CharField(max_length=200,null=True)
+    address = models.CharField(max_length=200,null=True)
+    price = models.CharField(max_length=200,null=True)
+    table = models.CharField(max_length=200,null=True)
+    review = models.CharField(max_length=200,null=True)
+    rate = models.FloatField()
+    link = models.CharField(max_length=200,null=True)
+    image = models.ImageField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
+    @property
+    def ImageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url=''
+        return url
+class TableType(models.Model):
+    name_table = models.CharField(max_length=100,unique=True,null=True)
+    
+    def __str__(self):
+        return self.name_table
+    
--- app/views.py
+++ app/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def home(request):
-    return render(request, 'app/home.html')
+    clubs = Club.objects.all()
+    districts = (Club.objects
+                 .values_list('district', flat=True)
+                 .filter(district__isnull=False)
+                 .order_by('district')
+                 .distinct())
+    table_types = (TableType.objects.values_list('name_table',flat=True).filter(name_table__isnull=False).order_by('name_table').distinct())
+    return render(request, 'app/home.html', {
+        'clubs': clubs,
+        'districts': districts,
+        'table_types': table_types,
+    })
+    
