2fadc1eb26779f347b3fd6d273051f096e330ac7
thanhdat2k2 <thanhdat2k2.810@gmail.com>
2025-11-09 13:06:53 +0700

    change database

--- Billiards_Hub_v1/settings.py
+++ Billiards_Hub_v1/settings.py
@@ -80,11 +80,17 @@ WSGI_APPLICATION = 'Billiards_Hub_v1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 
