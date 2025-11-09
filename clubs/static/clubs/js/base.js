3754122634d5347f1bdd5497ce045685348c9d0e
thanhdat2k2 <thanhdat2k2.810@gmail.com>
2025-11-09 15:09:20 +0700

    update notification message

--- clubs/static/clubs/js/base.js
+++ clubs/static/clubs/js/base.js
@@ -7,3 +7,23 @@ document.addEventListener("DOMContentLoaded", () => {
     navbar.classList.toggle("active");
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const toasts = document.querySelectorAll(".toast");
+  toasts.forEach((toast, index) => {
+    // hiệu ứng show
+    setTimeout(() => {
+      toast.classList.add("show");
+    }, 50 + index * 100);
+
+    // auto ẩn sau 4s
+    setTimeout(() => {
+      toast.classList.remove("show");
+    }, 4000 + index * 200);
+
+    // nút X
+    toast.querySelector(".toast-close").addEventListener("click", () => {
+      toast.classList.remove("show");
+    });
+  });
+});
